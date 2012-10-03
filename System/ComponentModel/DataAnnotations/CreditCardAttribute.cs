1d750c75d446fde358c0eae4d214ff83cdd43b50
erashid <ehsan.rashid@gmail.com>
2012-10-03 14:39:28 +0500

    DataAnnotations

--- System/ComponentModel/DataAnnotations/CreditCardAttribute.cs
+++ System/ComponentModel/DataAnnotations/CreditCardAttribute.cs
@@ -64,4 +64,107 @@ namespace System.ComponentModel.DataAnnotations
             return (sum % 10 == 0);
         }
     }
+    /*
+     public class CreditCardAttribute : ValidationAttribute, IClientValidatable
+    {
+        private CardType _cardTypes;
+        public CardType AcceptedCardTypes
+        {
+            get { return _cardTypes; }
+            set { _cardTypes = value; }
+        }
+ 
+        public CreditCardAttribute()
+        {
+            _cardTypes = CardType.All;
+        }
+ 
+        public CreditCardAttribute(CardType acceptedCardTypes)
+        {
+            _cardTypes = acceptedCardTypes;
+        }
+ 
+        public override bool IsValid(object value)
+        {
+            var number = Convert.ToString(value);
+ 
+            if (String.IsNullOrEmpty(number))
+                return true;
+ 
+            return IsValidType(number, _cardTypes) && IsValidNumber(number);
+        }
+ 
+        public override string FormatErrorMessage(string name)
+        {
+            return "The " + name + " field contains an invalid credit card number.";
+        }
+ 
+        [Flags]
+        public enum CardType
+        {
+            Unknown = 1,
+            Visa = 2,
+            MasterCard = 4,
+            Amex = 8,
+            Diners = 16,
+ 
+            All = Visa | MasterCard | Amex | Diners,
+            AllOrUnknown = Unknown | Visa | MasterCard | Amex | Diners
+        }
+ 
+        private static bool IsValidType(string cardNumber, CardType cardType)
+        {
+            // Visa
+            if (Regex.IsMatch(cardNumber, "^(4)")
+                && ((cardType & CardType.Visa) != 0))
+                return cardNumber.Length == 13 || cardNumber.Length == 16;
+ 
+            // MasterCard
+            if (Regex.IsMatch(cardNumber, "^(51|52|53|54|55)")
+                && ((cardType & CardType.MasterCard) != 0))
+                return cardNumber.Length == 16;
+ 
+            // Amex
+            if (Regex.IsMatch(cardNumber, "^(34|37)")
+                && ((cardType & CardType.Amex) != 0))
+                return cardNumber.Length == 15;
+ 
+            // Diners
+            if (Regex.IsMatch(cardNumber, "^(300|301|302|303|304|305|36|38)")
+                && ((cardType & CardType.Diners) != 0))
+                return cardNumber.Length == 14;
+ 
+            //Unknown
+            if ((cardType & CardType.Unknown) != 0)
+                return true;
+ 
+            return false;
+        }
+ 
+        private static bool IsValidNumber(string number)
+        {
+            var deltas = new[] { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
+            var checksum = 0;
+            var chars = number.ToCharArray();
+            for (var i = chars.Length - 1; i > -1; i--)
+            {
+                var j = chars[i] - 48;
+                checksum += j;
+                if (((i - chars.Length) % 2) == 0)
+                    checksum += deltas[j];
+            }
+ 
+            return ((checksum % 10) == 0);
+        }
+ 
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            yield return new ModelClientValidationRule
+            {
+                ErrorMessage = ErrorMessage,
+                ValidationType = "creditcard"
+            };
+        }
+    }
+     */
 }
---
+++ System/ComponentModel/DataAnnotations/EqualToPropertyAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace System.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class EqualToPropertyAttribute : ValidationAttribute
+    {
+        public string CompareProperty { get; set; }
+
+        public EqualToPropertyAttribute(string compareProperty)
+        {
+            CompareProperty = compareProperty;
+            ErrorMessage = ""; //string.Format(Messages.EqualToError, compareProperty);
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (null == value) return true;
+
+            var properties = TypeDescriptor.GetProperties(value);
+            var property = properties.Find(CompareProperty, true);
+            var comparePropertyValue = property.GetValue(value).ToString();
+
+            return comparePropertyValue == value.ToString();
+        }
+    }
+}
