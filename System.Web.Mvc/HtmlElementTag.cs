714ed4af4c8f8174ed059e0284999ad07f5be24c
erashid <ehsan.rashid@gmail.com>
2012-10-13 16:16:38 +0500

    .

---
+++ System.Web.Mvc/HtmlElementTag.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Web.Mvc
+{
+    /// <summary>
+    /// Sets local type of HTML element that is used on an HTML wrapper
+    /// </summary>
+    public enum HtmlElementTag
+    {
+        None,
+        Tr,
+        Td,
+        Li,
+        Div,
+        Table,
+        Ul
+
+    }
+}
